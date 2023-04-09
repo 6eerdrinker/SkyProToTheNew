8408f165c93e8273369e4866cd17ed4fd854b1bb
ь <klempaleks@mail.ru>
2023-04-09 15:45:23 +0900

    Homework Cicles Lesson Two is done. Check please.

--- src/Probe.java
+++ src/Probe.java
@@ -1,8 +1,6 @@
 public class Probe {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            int a = i * 2;
-            System.out.println(2 + "*" + i + "=" + a);
-        }
+
+
     }
 }
